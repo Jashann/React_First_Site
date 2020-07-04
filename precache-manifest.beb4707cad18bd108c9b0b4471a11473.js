72c567ce253ba44162fafc7a0c12052f38d5693c
Jashann <48613250+Jashann@users.noreply.github.com>
2020-07-04 21:04:39 +0530

    Updates

---
+++ precache-manifest.beb4707cad18bd108c9b0b4471a11473.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1604cbf15e2f312ab32d4e6f23448cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3831cf78ff634093696",
+    "url": "/static/css/main.260cb761.chunk.css"
+  },
+  {
+    "revision": "1182796933cd1151d7a7",
+    "url": "/static/js/2.4138404b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4138404b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3831cf78ff634093696",
+    "url": "/static/js/main.625a8255.chunk.js"
+  },
+  {
+    "revision": "fea4c49c3b23c140bfb0",
+    "url": "/static/js/runtime-main.36dae93d.js"
+  }
+]);
